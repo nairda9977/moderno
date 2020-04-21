9aa88eb60a3c2309b34da5019a04130183acc99d
nairda9977 <nairda9977@gmail.com>
2020-04-21 20:21:08 +0100

    added author section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,41 @@
 $(function(){
    var mixer = mixitup('.products__inner-box');
+      
+    $(".rateYo").rateYo({
+    rating: 5.0,
+    starWidth: "15px", 
+        readOnly: true
+  });
     
-    
-    
-    
+    $(".product-slider__inner").slick({
+        arrows:false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll:4,
+         responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+    })
+  
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.css', 
          'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/normalize.css/normalize.css'
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
                     ])
     .pipe(concut('libs.min.css'))
     .pipe(cssmin())
@@ -51,7 +52,8 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js', 
          'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-         'node_modules/mixitup/dist/mixitup.js'
+         'node_modules/mixitup/dist/mixitup.js',
+         'node_modules/rateyo/src/jquery.rateyo.js'
                     ])
     .pipe(concut('libs.min.js'))
     .pipe(uglify())
